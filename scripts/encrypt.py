10dd7234e5d0411abf8345123031972b8a7510a2
Timo Welde <tjwelde@gmail.com>
2021-01-29 11:12:26 +0000

    chore: remove obsolete scripts

--- scripts/encrypt.py
+++
@@ -1,7 +0,0 @@
-import zymkey
-from pathlib import Path
-
-filepath = Path.cwd() / 'hello_world'
-filedest = Path.cwd() / 'hello_world2'
-
-zymkey.client.lock(str(filepath), str(filedest))
--- scripts/random.py
+++
@@ -1,6 +0,0 @@
-import zymkey
-
-num = 512
-random = zymkey.client.get_random(num)
-s = random.hex()
-print(s)
