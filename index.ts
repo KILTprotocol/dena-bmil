566ec6ca60f8377357f440d0ee8037be3011dc98
Timo Welde <tjwelde@gmail.com>
2021-03-24 13:37:43 +0100

    feat: initial express server with generating identity and registering did

--- index.ts
+++ index.ts
@@ -1,12 +1,93 @@
+import express, { ErrorRequestHandler } from 'express'
+import fetch from 'node-fetch'
+
 import Kilt from '@kiltprotocol/sdk-js'
 import { generateRandom, encryptAndStore, retrieveAndDecrypt } from './utils'
 
-generateRandom().then((random) => {
-  const identity = Kilt.Identity.buildFromURI(`0x${random}`)
-  console.log(identity.seedAsHex)
+Kilt.config({
+  address: 'wss://full-nodes.kilt.io',
 })
 
-encryptAndStore({ hello: 'Oli' })
-retrieveAndDecrypt().then((message) => {
-  console.log(message)
+const getStoredIdentity = async () => {
+  const store: any = await retrieveAndDecrypt()
+  return Kilt.Identity.buildFromURI(store.identity)
+}
+
+const app = express()
+const port = 3000
+
+app.post('/identity', (req, res) => {
+  generateRandom().then((random) => {
+    const identity = Kilt.Identity.buildFromURI(`0x${random}`)
+    encryptAndStore({ identity: identity.seedAsHex })
+    res.send(identity.address)
+  })
+})
+
+app.get('/identity', async (req, res) => {
+  const identity = await getStoredIdentity()
+  res.send(identity.address)
+})
+
+app.post('/identity/register', async (req, res, next) => {
+  try {
+    const identity = await getStoredIdentity()
+    const did = Kilt.Did.fromIdentity(identity)
+    const ddo = did.createDefaultDidDocument(
+      'https://services.kilt.io/messaging'
+    )
+
+    const signedDdo = Kilt.Did.signDidDocument(ddo, identity)
+
+    const contact = {
+      did: signedDdo,
+      metaData: {
+        name: signedDdo.id,
+      },
+      publicIdentity: identity.getPublicIdentity(),
+    }
+
+    const response = await fetch('https://services.kilt.io/contacts', {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
+      method: 'POST',
+      body: JSON.stringify(contact),
+    })
+
+    if (!response.ok) {
+      throw Error(
+        'Error while storing DID Document in services: ' + response.statusText
+      )
+    }
+
+    const tx = await did.store(identity)
+    const txResult = await Kilt.BlockchainUtils.submitSignedTx(tx, {
+      resolveOn: Kilt.BlockchainUtils.IS_IN_BLOCK,
+    })
+
+    res.json(ddo)
+  } catch (e) {
+    next(e)
+  }
+})
+
+// Error handler
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err.stack)
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack?.split('\n'),
+    })
+  }
+)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
