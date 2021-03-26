01312ce609526e9cccbe058d34f3cc41a3c7eb88
Timo Welde <tjwelde@gmail.com>
2021-03-26 10:57:00 +0100

    feat: add swagger

---
+++ swagger/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Run the project and access the documentation at: http://localhost:3000/doc
+ *
+ * Use the command below to generate the documentation without starting the project:
+ * $ npm start
+ *
+ * Use the command below to generate the documentation at project startup:
+ * $ npm run start-gendoc
+ */
+
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs');
+const express = require('express')
+const app = express()
+
+const swaggerFile = YAML.load(__dirname + '/swagger.yml');
+
+/* Middlewares */
+app.use(express.json())
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.listen(3000, () => {
+  console.log("Server is running!\nAPI documentation: http://localhost:3000/doc")
+})
+
+/* Endpoints */
+// require('./src/endpoints')(app)
