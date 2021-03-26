a1b2bcc5213de268ebbb421878c4cb2aa5922a9e
Timo Welde <tjwelde@gmail.com>
2021-03-26 18:30:23 +0100

    feat: use routes and do verification workflow

--- index.ts
+++ index.ts
@@ -1,178 +1,30 @@
 import express from 'express'
 import fetch from 'node-fetch'
 import Kilt, {
+  IAttestation,
   Identity,
   IEncryptedMessage,
   IPublicIdentity,
-  IRequestAttestationForClaim,
+  ISubmitClaimsForCTypes,
   Message,
 } from '@kiltprotocol/sdk-js'
-import { generateRandom, encryptAndStore, retrieveAndDecrypt } from './utils'
-
-const BASE_POST_PARAMS = {
-  headers: {
-    'Content-Type': 'application/json; charset=utf-8',
-  },
-  method: 'POST',
-}
-
-const BASE_DELETE_PARAMS = {
-  method: 'DELETE',
-}
-
-const BASE_URL = 'https://services.kilt.io'
-const MESSAGING_URL = `${BASE_URL}/messaging`
-
-Kilt.config({
-  address: 'wss://full-nodes.kilt.io',
-})
-
-const getStoredIdentity = async () => {
-  const store: any = await retrieveAndDecrypt()
-  return Kilt.Identity.buildFromURI(store.identity)
-}
-
-const storeRequest = async (request: any) => {
-  const store: any = await retrieveAndDecrypt()
-  const newStore = {
-    ...store,
-    request,
-  }
-  encryptAndStore(newStore)
-}
-
-const getStoredRequest = async () => {
-  const store: any = await retrieveAndDecrypt()
-  if (store.request) {
-    return Kilt.RequestForAttestation.fromRequest(store.request)
-  }
-}
-
-const storeCredential = async (credential: any) => {
-  const store: any = await retrieveAndDecrypt()
-  const newStore = {
-    ...store,
-    credential,
-  }
-  delete newStore.request
-  encryptAndStore(newStore)
-}
+import {
+  getStoredIdentity,
+  storeCredential,
+  getStoredRequest,
+  getStoredCredential,
+} from './utils/helper'
+import {
+  MESSAGING_URL,
+  BASE_DELETE_PARAMS,
+  BASE_POST_PARAMS,
+} from './utils/fetch'
+import routes from './routes'
 
 const app = express()
 const port = 3000
 
-app.post('/identity', (req, res) => {
-  generateRandom().then((random) => {
-    const identity = Kilt.Identity.buildFromURI(`0x${random}`)
-    encryptAndStore({ identity: identity.seedAsHex })
-    res.json(identity.address)
-  })
-})
-
-app.get('/identity', async (req, res) => {
-  const identity = await getStoredIdentity()
-  res.json(identity.address)
-})
-
-app.post('/identity/register', async (req, res, next) => {
-  try {
-    const identity = await getStoredIdentity()
-    const did = Kilt.Did.fromIdentity(identity)
-    const ddo = did.createDefaultDidDocument(MESSAGING_URL)
-
-    const signedDdo = Kilt.Did.signDidDocument(ddo, identity)
-
-    const contact = {
-      did: signedDdo,
-      metaData: {
-        name: signedDdo.id,
-      },
-      publicIdentity: identity.getPublicIdentity(),
-    }
-
-    const response = await fetch('https://services.kilt.io/contacts', {
-      ...BASE_POST_PARAMS,
-      body: JSON.stringify(contact),
-    })
-
-    if (!response.ok) {
-      throw Error(
-        'Error while storing DID Document in services: ' + response.statusText
-      )
-    }
-
-    const tx = await did.store(identity)
-    const txResult = await Kilt.BlockchainUtils.submitSignedTx(tx, {
-      resolveOn: Kilt.BlockchainUtils.IS_IN_BLOCK,
-    })
-
-    res.json(ddo)
-  } catch (e) {
-    next(e)
-  }
-})
-
-const ctype = Kilt.CType.fromCType({
-  schema: {
-    $id:
-      'kilt:ctype:0x5366521b1cf4497cfe5f17663a7387a87bb8f2c4295d7c40f3140e7ee6afc41b',
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'DriversLicense',
-    properties: {
-      name: {
-        type: 'string',
-      },
-      age: {
-        type: 'integer',
-      },
-    },
-    type: 'object',
-  },
-  owner: '4tf4mVShDTGaqZCRHjhH3Wyg9iXuGkKi14mRFVbvq5Tnmk7J',
-  hash: '0x5366521b1cf4497cfe5f17663a7387a87bb8f2c4295d7c40f3140e7ee6afc41b',
-})
-
-const attester: IPublicIdentity = {
-  address: '4qK4AzHMBQVGKUURU2AJq9Ch8ozgSFEngmo87S7cggqK6kJW',
-  boxPublicKeyAsHex:
-    '0x57172b4336a091a06e0a1c5dbbd2a3a70ff953f2bb4a9de439ab4397f734d714',
-  serviceAddress: MESSAGING_URL,
-}
-
-app.post('/claim', async (req, res) => {
-  const identity = await getStoredIdentity()
-  const claim = Kilt.Claim.fromCTypeAndClaimContents(
-    ctype,
-    {
-      name: 'OLI',
-      age: 20,
-    },
-    identity.address
-  )
-
-  const requestForAttestation = Kilt.RequestForAttestation.fromClaimAndIdentity(
-    claim,
-    identity
-  )
-
-  const messageBody: IRequestAttestationForClaim = {
-    content: { requestForAttestation },
-    type: Kilt.MessageBodyType.REQUEST_ATTESTATION_FOR_CLAIM,
-  }
-  const message = new Message(messageBody, identity, attester)
-  const encrypted = message.encrypt()
-
-  const response = await fetch(MESSAGING_URL, {
-    ...BASE_POST_PARAMS,
-    body: JSON.stringify(encrypted),
-  })
-
-  if (response.ok) {
-    await storeRequest(requestForAttestation)
-  }
-
-  res.json('Finished')
-})
+app.use(routes)
 
 // Error handler
 app.use(
@@ -198,29 +50,67 @@ const deleteMessage = (messageId: string, identity: Identity) => {
   })
 }
 
+const handleAttestationMessage = async (attestation: IAttestation) => {
+  console.log(attestation)
+  const request = await getStoredRequest()
+  if (request) {
+    const credential = Kilt.AttestedClaim.fromRequestAndAttestation(
+      request,
+      attestation
+    )
+    await storeCredential(credential)
+  }
+}
+
+const handleRequestClaimMessage = async (
+  ctypes: string[],
+  identity: Identity,
+  verifier: IPublicIdentity
+) => {
+  const credential = await getStoredCredential()
+  const foundCtype = ctypes.find((ctypeHash) => {
+    if (credential?.request.claim.cTypeHash === ctypeHash) {
+      return credential
+    }
+  })
+
+  if (foundCtype && credential) {
+    const messageBody: ISubmitClaimsForCTypes = {
+      content: [credential],
+      type: Kilt.Message.BodyType.SUBMIT_CLAIMS_FOR_CTYPES,
+    }
+    const message = new Message(messageBody, identity, verifier)
+    const encrypted = message.encrypt()
+
+    const response = await fetch(MESSAGING_URL, {
+      ...BASE_POST_PARAMS,
+      body: JSON.stringify(encrypted),
+    })
+  }
+}
+
 const handleMessages = async (messages: IEncryptedMessage[]) => {
   const identity = await getStoredIdentity()
   messages.forEach(async (encrypted) => {
     const decryted = Kilt.Message.decrypt(encrypted, identity)
-    if (
-      decryted.body.type === Kilt.MessageBodyType.SUBMIT_ATTESTATION_FOR_CLAIM
-    ) {
-      const { attestation } = decryted.body.content
-      console.log(attestation)
-      const request = await getStoredRequest()
-      try {
-        if (request) {
-          const credential = Kilt.AttestedClaim.fromRequestAndAttestation(
-            request,
-            attestation
-          )
-          storeCredential(credential)
-        }
-      } catch (e) {
-        throw e
-      } finally {
-        if (decryted.messageId) deleteMessage(decryted.messageId, identity)
+    try {
+      switch (decryted.body.type) {
+        case Kilt.Message.BodyType.SUBMIT_ATTESTATION_FOR_CLAIM:
+          const { attestation } = decryted.body.content
+          handleAttestationMessage(attestation)
+          break
+        case Kilt.Message.BodyType.REQUEST_CLAIMS_FOR_CTYPES:
+          const { ctypes } = decryted.body.content
+          handleRequestClaimMessage(ctypes, identity, {
+            address: decryted.senderAddress,
+            boxPublicKeyAsHex: decryted.senderBoxPublicKey
+          })
+          break
       }
+    } catch (e) {
+      throw e
+    } finally {
+      if (decryted.messageId) deleteMessage(decryted.messageId, identity)
     }
   })
 }
---
+++ routes.ts
@@ -0,0 +1,143 @@
+import express from 'express'
+import fetch from 'node-fetch'
+import Kilt, {
+  IPublicIdentity,
+  IRequestAttestationForClaim,
+  Message,
+} from '@kiltprotocol/sdk-js'
+import {
+  getStoredCredential,
+  getStoredIdentity,
+  storeRequest,
+} from './utils/helper'
+import { MESSAGING_URL, BASE_POST_PARAMS, CONTACTS_URL } from './utils/fetch'
+import { generateRandom, encryptAndStore } from './utils/crypto'
+
+Kilt.config({
+  address: 'wss://full-nodes.kilt.io',
+})
+
+const router = express.Router()
+
+router.post('/identity', (req, res) => {
+  generateRandom().then((random) => {
+    const identity = Kilt.Identity.buildFromURI(`0x${random}`, {
+      signingKeyPairType: 'ed25519',
+    })
+    encryptAndStore({ identity: identity.seedAsHex })
+    res.json(identity.address)
+  })
+})
+
+router.get('/identity', async (req, res) => {
+  const identity = await getStoredIdentity()
+  res.json(identity.address)
+})
+
+router.post('/identity/register', async (req, res, next) => {
+  try {
+    const identity = await getStoredIdentity()
+    const documentStore = `${CONTACTS_URL}/${identity.address}`
+    const did = Kilt.Did.fromIdentity(identity, documentStore)
+    const ddo = did.createDefaultDidDocument(MESSAGING_URL)
+
+    const signedDdo = Kilt.Did.signDidDocument(ddo, identity)
+
+    const contact = {
+      did: signedDdo,
+      metaData: {
+        name: signedDdo.id,
+      },
+      publicIdentity: identity.getPublicIdentity(),
+    }
+
+    const response = await fetch('https://services.kilt.io/contacts', {
+      ...BASE_POST_PARAMS,
+      body: JSON.stringify(contact),
+    })
+
+    if (!response.ok) {
+      throw Error(
+        'Error while storing DID Document in services: ' + response.statusText
+      )
+    }
+
+    const tx = await did.store(identity)
+    const txResult = await Kilt.BlockchainUtils.submitSignedTx(tx, {
+      resolveOn: Kilt.BlockchainUtils.IS_IN_BLOCK,
+    })
+
+    res.json(ddo)
+  } catch (e) {
+    next(e)
+  }
+})
+
+const ctype = Kilt.CType.fromCType({
+  schema: {
+    $id:
+      'kilt:ctype:0x5366521b1cf4497cfe5f17663a7387a87bb8f2c4295d7c40f3140e7ee6afc41b',
+    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
+    title: 'DriversLicense',
+    properties: {
+      name: {
+        type: 'string',
+      },
+      age: {
+        type: 'integer',
+      },
+    },
+    type: 'object',
+  },
+  owner: '4tf4mVShDTGaqZCRHjhH3Wyg9iXuGkKi14mRFVbvq5Tnmk7J',
+  hash: '0x5366521b1cf4497cfe5f17663a7387a87bb8f2c4295d7c40f3140e7ee6afc41b',
+})
+
+const attester: IPublicIdentity = {
+  address: '4qK4AzHMBQVGKUURU2AJq9Ch8ozgSFEngmo87S7cggqK6kJW',
+  boxPublicKeyAsHex:
+    '0x57172b4336a091a06e0a1c5dbbd2a3a70ff953f2bb4a9de439ab4397f734d714',
+  serviceAddress: MESSAGING_URL,
+}
+
+router.post('/claim', async (req, res) => {
+  const identity = await getStoredIdentity()
+  const claim = Kilt.Claim.fromCTypeAndClaimContents(
+    ctype,
+    {
+      name: 'OLI',
+      age: 20,
+    },
+    identity.address
+  )
+
+  const requestForAttestation = Kilt.RequestForAttestation.fromClaimAndIdentity(
+    claim,
+    identity
+  )
+
+  const messageBody: IRequestAttestationForClaim = {
+    content: { requestForAttestation },
+    type: Kilt.Message.BodyType.REQUEST_ATTESTATION_FOR_CLAIM,
+  }
+  const message = new Message(messageBody, identity, attester)
+  const encrypted = message.encrypt()
+
+  const response = await fetch(MESSAGING_URL, {
+    ...BASE_POST_PARAMS,
+    body: JSON.stringify(encrypted),
+  })
+
+  if (response.ok) {
+    await storeRequest(requestForAttestation)
+  }
+
+  res.json('Finished')
+})
+
+router.get('/credential', async (req, res) => {
+  const credential = await getStoredCredential()
+  res.json(credential)
+})
+
+export default router
