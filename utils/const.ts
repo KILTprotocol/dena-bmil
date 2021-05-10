145670872c1a54e1e7fb92d3bdad36e057d87135
Timo Welde <tjwelde@gmail.com>
2021-05-10 18:12:11 +0200

    feat: new const values

--- utils/const.ts
+++ utils/const.ts
@@ -2,13 +2,13 @@ import Kilt, { IPublicIdentity } from '@kiltprotocol/sdk-js'
 import { MESSAGING_URL } from './fetch'
 
 export const attester: IPublicIdentity = {
-  address: '4tb4hAdiN9C5L4ycwFLR7wGcQrx4r8qSh15RFACwNwDmAeM5',
+  address: '4oTwFGDLgK4nUpnVTYkW8rAYxCG3Noedhcz5YNzWsoxwU3J1',
   boxPublicKeyAsHex:
-    '0x0f7cece0e9ce82743d889e7d2ca1c54896d4d5bb942226176d80234bb0ebb04c',
+    '0xd507eda0658bd2f1a170143525879c343b0635307ed2d17daf4c07e8b161e637',
   serviceAddress: MESSAGING_URL,
 }
 
-export const delegationRootId = "0xe5059302753ec79d6cbfaffdbd1f6e0eb55a6c016d94af10d224014242173b95"
+export const delegationRootId = "0x7d65d67d6bb547e6f5df19be7fc21593ee374ea34730c21ea3a623b042de56c4"
 
 export const excludedClaimProperties = ['Laengengrad', 'Breitengrad']
 
