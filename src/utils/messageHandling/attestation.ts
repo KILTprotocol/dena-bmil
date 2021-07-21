e2ced1f3c7a7fa47d45debf6cab8ab48622a9fb9
Timo Welde <tjwelde@gmail.com>
2021-07-21 18:23:14 +0200

    feat: export to vc and POST to ewf

--- src/utils/messageHandling/attestation.ts
+++ src/utils/messageHandling/attestation.ts
@@ -1,12 +1,9 @@
-import Kilt, {
-  IAttestation,
-} from '@kiltprotocol/sdk-js'
-import {
-  storeCredential,
-  getStoredRequest,
-  store,
-  retrieve,
-} from '../helper'
+import Kilt, { IAttestation } from '@kiltprotocol/sdk-js'
+import VCUtils from '@kiltprotocol/vc-export'
+import fetch from 'node-fetch'
+import { BASE_POST_PARAMS } from '../fetch'
+import { storeCredential, getStoredRequest, store, retrieve } from '../helper'
+import { OLIBoxCredentialCtype } from '../const'
 
 export const handleAttestationMessage = async (attestation: IAttestation) => {
   console.log(attestation)
@@ -20,7 +17,34 @@ export const handleAttestationMessage = async (attestation: IAttestation) => {
     )
     await storeCredential(credential)
     console.log('👍 Credential stored!')
-  } else if (energyWebRequest && energyWebRequest.rootHash === attestation.claimHash) {
+
+    console.log('📤 exporting to VC and sending to the EWF app')
+    const VC = VCUtils.fromAttestedClaim(credential, OLIBoxCredentialCtype)
+    // TODO: make a presentation?
+
+    VC.type = [...VC.type, 'BMILInstallationCredential']
+
+    const wrapForEwf = {
+      subject_did: VC.credentialSubject['@id'],
+      credential: VC,
+    }
+
+    console.log(JSON.stringify(wrapForEwf, undefined, 4))
+
+    const response = await fetch(
+      'https://9d0761767845.ngrok.io/bmil-backend/bmil/credentials',
+      { ...BASE_POST_PARAMS, body: JSON.stringify(wrapForEwf) }
+    )
+
+    if (response.ok) {
+      console.log('👍 Credential sent!')
+    } else {
+      console.error(response.status, response.statusText)
+    }
+  } else if (
+    energyWebRequest &&
+    energyWebRequest.rootHash === attestation.claimHash
+  ) {
     console.log(
       '✅ Attestation matches previous Request: EnergyWeb Role Credential'
     )
